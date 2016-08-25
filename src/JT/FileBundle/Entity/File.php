aa33b6ff2fe6e63278a16b8a795b7116f93c8a19
Jimmy Tournemaine <jimmy.tournemaine@yahoo.fr>
2016-08-25 16:28:02 +0200

    File Upload Handling

---
+++ src/JT/FileBundle/Entity/File.php
@@ -0,0 +1,21 @@
+<?php
+namespace JT\FileBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JT\FileBundle\Model\File as BaseFile;
+
+/**
+ * @ORM\MappedSuperclass
+ */
+class File extends BaseFile
+{
+	/**
+	 * @ORM\Column(name="original")
+	 */
+	protected originalName;
+
+	/**
+	 * @ORM\Column(name="filename")
+	 */
+	protected filename;
+}
