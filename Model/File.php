b9406f1ef1cbc05ff3cf7bc9d4c53515806fbb8c
Jimmy Tournemaine <jimmy.tournemaine@yahoo.fr>
2016-08-25 16:36:59 +0200

    Initial commit

---
+++ Model/File.php
@@ -0,0 +1,49 @@
+<?php
+namespace JT\FileBundle\Model;
+
+use Symfony\Component\HttpFoundation\File\File;
+
+class File implements UploadableFile
+{
+	protected $originalName;
+	protected $filename;
+	protected $file;
+
+	public function getAbsolutePathname()
+	{
+		return $this->getTargetDirectory() . '/' . $this->getFilename();
+	}
+
+	public function setOriginalName($name)
+	{
+		$this->originalName = $name;
+		return $this;
+	}
+
+	public function getOriginalName()
+	{
+		return $this->originalName;
+	}
+
+	public function setFilename($filename)
+	{
+		$this->filename = $filename;
+		return $this;
+	}
+
+	public function getFilename()
+	{
+		return $this->filename;
+	}
+
+	public function getFile()
+	{
+		if($this->file !== null){
+			return $this->file;
+		}
+
+		return $this->file = new File($this->getAbsolutePathname(), true)
+	}
+}
+
+
